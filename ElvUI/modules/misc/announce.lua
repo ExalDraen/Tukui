2d15370f06fa4b356e8740a3afd0592dd0eeeee3
unknown <Alex@.(none)>
2011-02-26 19:20:59 +0000

    Make announce channel customisable, Part III

--- ElvUI/modules/misc/announce.lua
+++ ElvUI/modules/misc/announce.lua
@@ -7,9 +7,15 @@ announce:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 announce:SetScript("OnEvent", function(self, _, _, event, _, sourceName, _, _, destName, _, _, _, _, spellID, spellName)
 	if not (event == "SPELL_INTERRUPT" and sourceName == E.myname) then return end
 	
-	if GetNumPartyMembers() > 0 then
-		SendChatMessage(INTERRUPTED.." "..destName.."'s \124cff71d5ff\124Hspell:"..spellID.."\124h["..spellName.."]\124h\124r!", C["others"].announcechannel, nil, nil)
-	elseif GetNumRaidMembers() > 0 then
+	if (C["others"].announcechannel == "PARTY" or C["others"].announcechannel== "RAID")
+	then		
+		if GetNumPartyMembers() > 0 then
+			SendChatMessage(INTERRUPTED.." "..destName.."'s \124cff71d5ff\124Hspell:"..spellID.."\124h["..spellName.."]\124h\124r!", "PARTY", nil, nil)
+		elseif GetNumRaidMembers() > 0 then
+			SendChatMessage(INTERRUPTED.." "..destName.."'s \124cff71d5ff\124Hspell:"..spellID.."\124h["..spellName.."]\124h\124r!", "RAID", nil, nil)
+		end
+	else
 		SendChatMessage(INTERRUPTED.." "..destName.."'s \124cff71d5ff\124Hspell:"..spellID.."\124h["..spellName.."]\124h\124r!", C["others"].announcechannel, nil, nil)
 	end
+		
 end)
