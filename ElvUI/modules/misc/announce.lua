6b3240d0dc823c7d48d40229654499ebdb7f5793
ACH <Night_Lynx@hotmail.com>
2011-04-03 17:53:12 +0100

    fix <<<<HEad left in announce luja file
    Signed-off-by: ACH <Night_Lynx@hotmail.com>

--- ElvUI/modules/misc/announce.lua
+++ ElvUI/modules/misc/announce.lua
@@ -7,7 +7,6 @@ announce:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 announce:SetScript("OnEvent", function(self, _, _, event, _, sourceName, _, _, destName, _, _, _, _, spellID, spellName)
 	if not (event == "SPELL_INTERRUPT" and sourceName == E.myname) then return end
 	
-<<<<<<< HEAD
 	if (C["others"].announcechannel == "PARTY" or C["others"].announcechannel== "RAID")
 	then		
 		if GetRealNumPartyMembers() > 0 then
